192759e6b2e0ae53947161eda0a95950e12d552e
Niklas <nyholmniklas@gmail.com>
2014-04-01 21:57:17 +0300

    Added openshift profile to maven build

---
+++ src/main/java/org/niklas/finvoice2csv/util/Model2CsvMapperImp.java
@@ -0,0 +1,15 @@
+package org.niklas.finvoice2csv.util;
+
+import java.io.File;
+
+import org.niklas.finvoice2csv.model.Finvoice;
+
+public class Model2CsvMapperImp implements Model2CsvMapper {
+
+	@Override
+	public void createCsvFileFromFinvoice(Finvoice finvoice, File file) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
